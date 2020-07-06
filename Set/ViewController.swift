bfb6fcf7c195f6ad4ec24f41f623f1706bd87daf
Minho-Choi <2153373@naver.com>
2020-07-06 14:45:08 +0900

    Initial Commit

---
+++ Set/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Set
+//
+//  Created by Minho Choi on 2020/07/06.
+//  Copyright © 2020 Minho Choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
